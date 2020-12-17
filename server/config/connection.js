b723c9c128e43ef28770e7db200f1581f2d77939
daejo <daejo@icloud.com>
2020-12-17 04:37:12 -0800

    updates file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'https://protected-tundra-83653.herokuapp.com/', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://deep-thoughts:dwakin2008@cluster0-shard-00-00.5srma.mongodb.net:27017,cluster0-shard-00-01.5srma.mongodb.net:27017,cluster0-shard-00-02.5srma.mongodb.net:27017/deep-thoughts?ssl=true&replicaSet=atlas-sbs2rx-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
