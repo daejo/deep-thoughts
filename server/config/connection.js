ed81c6abdf2a832042f279e94b1c30e94b511a17
daejo <daejo@icloud.com>
2020-12-17 17:24:57 -0800

    tests 1

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,8 +7,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://deep-thoughts:dwakin2008@
   useFindAndModify: false
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 module.exports = mongoose.connection;
